9c76f19c1f88e2097ad4c02c3f211d7015a6a549
kevinsilalahi <kronituasilalahi@gmail.com>
2023-11-21 16:54:57 +0700

    menambahkan paragraf

---
+++ OneDrive/Desktop/wpu-git-test-2/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur, sunt iure qui cum, ad quo nam voluptas, nisi consectetur deserunt nulla excepturi doloribus optio maxime veritatis sapiente. Suscipit, tempora officia.</p>
+</body>
+</html>
